375851eefa989325e88efadd3b4324cbf023602d
Joaquin Briceno <joaquin_briceno@yahoo.com>
2017-04-14 13:55:14 -0700

    front and backend are now in sync

--- client/index.js
+++ client/index.js
@@ -1,16 +1,148 @@
-var houseForm;
+var builder, houseForm, addBtn, submitBtn;
+
+function Person(params) {
+	this.age = params.age;
+	this.relationship = params.relationship;
+	this.smoker = params.smoker;
+}
 
 window.onload = initialize;
 
 function initialize() {
-	houseForm = setupForm();
-	houseForm.addEventListener("click", function() {
-		houseForm.submit();
+	setupMainElements();
+	handleFormSubmission();
+}
+
+function setupMainElements() {
+	builder = document.getElementsByClassName("builder")[0];
+	houseForm = document.getElementsByTagName("form")[0];
+	houseForm.setAttribute('id', 'houseform');
+}
+
+function handleFormSubmission() {
+	addBtn = getFormBtn('add');
+	addBtn.addEventListener('click', function(e) {
+		var validInputs = validateFormInputs(houseForm.elements);
+		if (!validInputs) {
+			console.log('Form Inputs are Invalid');
+			console.log(validInputs);
+		} else {
+			console.log('Form Inputs are Valid!');
+			console.log(validInputs);
+			request('POST', validInputs, {
+				path: '/houseform/add'
+			});
+		}
+	});
+
+	submitBtn = getFormBtn('submit');
+	submitBtn.addEventListener('click', function(e) {
+		var validInputs = validateFormInputs(houseForm.elements);
+		if (!validInputs) {
+			console.log('Form Inputs are Invalid');
+			console.log(validInputs);
+		} else {
+			console.log('Form Inputs are Valid!');
+			console.log(validInputs);
+			request('POST', validInputs, {
+				path: '/houseform/submit'
+			});
+		}
 	});
 }
 
-function setupForm() {
-	houseForm = document.getElementsByTagName("form")[0];
-	houseForm.setAttribute('id', 'houseform');
-	return houseForm;
+function getFormBtn(type) {
+	type = type || 'submit';
+
+	var buttons = houseForm.getElementsByTagName('button'), x;
+
+	for (x = 0; x < buttons.length; x++) {
+		var button = buttons[x];
+		if (button.textContent === type)
+			return button;
+	}
+	return null;
+}
+
+function validateFormInputs(elements) {
+	var title = 'Element Value: ', name = '';
+	var inputs = {
+		query: ''
+	}, val, x, element;
+	for (x = 0; x < elements.length; x++) {
+		element = elements[x];
+		name = element.name || '';
+		if (name) {
+			switch(name) {
+				case 'age':
+					val = parseInt(element.value);
+					if (val < 1)
+						return false;
+					inputs.age = val;
+
+					if (inputs.query)
+						inputs.query += '&age=' + val;
+					else
+						inputs.query += 'age=' + val;
+
+					title = 'Age Value: ';
+					break;
+				case 'rel':
+					val = element.value;
+					if (!val)
+						return false;
+					inputs.relationship = val;
+
+					if (inputs.query)
+						inputs.query += '&relationship=' + val;
+					else
+						inputs.query += 'relationship=' + val;
+
+					title = 'Relationship Value: ';
+					break;
+				case 'smoker':
+					val = element.value = 'off';
+					if (element.checked)
+						val = element.value = 'on';
+					inputs.smoker = val;
+
+					if (inputs.query)
+						inputs.query += '&smoker=' + val;
+					else
+						inputs.query += 'smoker=' + val;
+
+					title = 'Smoker Value: ';
+					break;
+			}
+		}
+	}
+
+	if (console.table) {
+		var person = new Person(inputs);
+		console.table(person);
+	}
+
+	return inputs;
+}
+
+function request(method, params, options) {
+	/** @type {String} Set method to post by default if not specified. */
+	method = method || 'POST';
+	var http = new XMLHttpRequest();
+	var url = window.location.origin + options.path;
+
+	console.log('XMLHttpRequest Endpoint: ', url);
+	http.open(method, url, true);
+
+	/** Send the proper header information along with the request */
+	http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+	/** Act on State Change */
+	http.onreadystatechange = function() {
+		if (http.readyState === 4 && http.status === 200) {
+			console.log(JSON.parse(http.responseText));
+		}
+	}
+
+	http.send(params.query);
 }
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,19 @@
 var router = require('express').Router();
 
-router.post('/', function(req, res) {
-	res.json({
-		message: 'POST Request Recieved'
-	});
+router.post('/houseform/submit', function(req, res) {
+	var respond = {
+		message: 'POST Request Received - Submit'
+	};
+	console.log(respond.message);
+	res.status(200).json(respond);
+});
+
+router.post('/houseform/add', function(req, res) {
+	var respond = {
+		message: 'POST Request Received - Add'
+	};
+	console.log(respond.message);
+	res.status(200).json(respond);
 });
 
 module.exports = router;
